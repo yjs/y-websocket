7945a5ba12aebbdcff9ff4fded7f0a7d3ccd7862
Kevin Jahns <kevin.jahns@protonmail.com>
2024-03-27 16:10:00 +0100

    add tips for handling auth and add typings for ws

--- bin/server.cjs
+++ bin/server.cjs
@@ -1,8 +1,5 @@
 #!/usr/bin/env node
 
-/**
- * @type {any}
- */
 const WebSocket = require('ws')
 const http = require('http')
 const number = require('lib0/number')
@@ -20,9 +17,9 @@ const server = http.createServer((_request, response) => {
 wss.on('connection', setupWSConnection)
 
 server.on('upgrade', (request, socket, head) => {
-  // You may check auth of request here.. Call `wss.HandleUpgrade` *after* you
-  // checked whether the client has access (e.g. by checking cookies, or url
-  // parameters).
+  // You may check auth of request here..
+  // Call `wss.HandleUpgrade` *after* you checked whether the client has access
+  // (e.g. by checking cookies, or url parameters).
   // See https://github.com/websockets/ws#client-authentication
   wss.handleUpgrade(request, socket, head, /** @param {any} ws */ ws => {
     wss.emit('connection', ws, request)
